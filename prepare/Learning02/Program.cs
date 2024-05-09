31427e19adfbd1d7a5f817d5a7d6ae662639e74e
JulioCesarPulgar <pulgarjuliocesar@gmail.com>
2024-05-09 19:54:45 -0400

    Update03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create job instances
+        Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022);
+        Job job2 = new Job("Apple", "Manager", 2022, 2023);
+        Job job3 = new Job("MundoTotal", "Web Developer", 2022, DateTime.Now.Year);
+
+        // Create resume instance
+        Resume myResume = new Resume("Daniel Rodriguez");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+        myResume.AddJob(job3);
+
+        // Display resume
+        myResume.Display();
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,36 @@
-using System;
-
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int top;
+    private int bottom;
+
+    public Fraction()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        this.top = 1;
+        this.bottom = 1;
     }
-}
+
+    public Fraction(int top)
+    {
+        this.top = top;
+        this.bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return this.top;
+    }
+
+    public void SetTop(int top)
+    {
+        this.top = top;
+    }
+
+    public int GetBottom()
+    {
+        return this.bottom;
